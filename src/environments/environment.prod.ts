cd688021f8e56c647966b421183b1f7d1182550a
Juan Pablo <jplantes@gmail.com>
2020-05-10 18:06:49 -0300

    Correcciones GitHub Pges

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,3 @@
 export const environment = {
-  production: true,
-  "output-path": "./docs/",
-  "base-href": "https://elcascarudo-dev.github.io/NavBar-responsibe-tailwindcss-angular9/"
+  production: true
 };
